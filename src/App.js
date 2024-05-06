2d5e1698ebfad6bfe0c3dafc9b194aefcfa846e9
nihilycho <nihilycho@gmail.com>
2024-05-06 20:32:57 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+import { WalletProvider, useWallet } from '@demox-labs/aleo-wallet-adapter-react';
+import { WalletModalProvider, WalletModalButton } from '@demox-labs/aleo-wallet-adapter-reactui';
+import { LeoWalletAdapter } from '@demox-labs/aleo-wallet-adapter-leo';
+import { DecryptPermission, WalletAdapterNetwork } from '@demox-labs/aleo-wallet-adapter-base';
+
+import '@demox-labs/aleo-wallet-adapter-reactui/styles.css';
+
+const WalletButton = () => {
+    const { connected, publicKey, disconnect } = useWallet();
+    const handleDisconnect = () => {
+        if (connected) {
+            disconnect();
+        }
+    };
+
+    if (connected && publicKey) {
+        return (
+            <button onClick={handleDisconnect}>
+                {publicKey.toString()} (Disconnect)
+            </button>
+        );
+    } else {
+        return (
+            <WalletModalButton>
+                Connect Leo Wallet
+            </WalletModalButton>
+        );
+    }
+};
 
 function App() {
+  const wallets = useMemo(() => [
+    new LeoWalletAdapter({
+      appName: "Leo Demo App", 
+    })
+  ], []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WalletProvider
+      wallets={wallets}
+      decryptPermission={DecryptPermission.UponRequest}
+      network={WalletAdapterNetwork.Localnet} 
+      autoConnect
+    >
+      <WalletModalProvider>
+        <div className="App">
+          <header className="App-header">
+            <WalletButton />
+          </header>
+        </div>
+      </WalletModalProvider>
+    </WalletProvider>
   );
 }
 
